b78bf04be93aba5aeeb9809660a3cd0ca24ec142
Michael Bridgen <michael.bridgen@nscale.com>
2025-11-12 12:10:14 +0000

    Factor out reader prep entirely
    This commit factors out a set of helpers for making readers, that's
    enough to cover all the uses in the test cases. The end result is that
    the test func doesn't need any nolint annotation, because it's simple
    enough.
    
    One side-effect: the test case for context cancellation no longer
    passes, which it shouldn't because I removed the context-aware
    reader. It was passing by coincidence because the "disk" context was
    invalid anyway (making it fail as expected).

--- pkg/handler/region/region_test.go
+++ pkg/handler/region/region_test.go
@@ -40,7 +40,88 @@ import (
 	"k8s.io/utils/ptr"
 )
 
-//gnolint:gocognit
+func stringReader(content string) func(t *testing.T) io.Reader {
+	return func(t *testing.T) io.Reader {
+		t.Helper()
+
+		return bytes.NewBufferString(content)
+	}
+}
+
+func base64Reader(t *testing.T, content string) io.Reader {
+	t.Helper()
+
+	bs, err := base64.StdEncoding.DecodeString(content)
+	require.NoError(t, err)
+
+	return bytes.NewBuffer(bs)
+}
+
+// foo takes a reader of file content and returns a ReaderSetupFunc yielding gzipped file content.
+func gzippedReader(content io.Reader) func(t *testing.T) io.Reader {
+	return func(t *testing.T) io.Reader {
+		t.Helper()
+
+		var buf bytes.Buffer
+
+		gzipWriter := gzip.NewWriter(&buf)
+
+		if _, err := io.Copy(gzipWriter, content); err != nil {
+			require.NoError(t, err)
+		}
+
+		if err := gzipWriter.Close(); err != nil {
+			require.NoError(t, err)
+		}
+
+		return &buf
+	}
+}
+
+type files map[string][]byte
+
+// tarballedReader takes a map of files, and returns a ReaderSetupFunc yielding a gzipped tarball.
+func tarballedReader(fs files) func(t *testing.T) io.Reader {
+	return func(t *testing.T) io.Reader {
+		t.Helper()
+
+		var buf bytes.Buffer
+
+		gzipWriter := gzip.NewWriter(&buf)
+
+		tarWriter := tar.NewWriter(gzipWriter)
+
+		for filename, content := range fs {
+			tarHeader := &tar.Header{
+				Name: filename,
+				Mode: 0600,
+				Size: int64(len(content)),
+			}
+
+			require.NoError(t, tarWriter.WriteHeader(tarHeader))
+
+			_, err := tarWriter.Write(content)
+			require.NoError(t, err)
+		}
+
+		require.NoError(t, tarWriter.Close())
+		require.NoError(t, gzipWriter.Close())
+
+		return &buf
+	}
+}
+
+func expectedReaderBytes(content []byte) gomock.Matcher {
+	return gomock.Cond(func(reader io.Reader) bool {
+		actual, err := io.ReadAll(reader)
+		if err != nil {
+			return false
+		}
+
+		return bytes.Equal(actual, content)
+	})
+}
+
 func TestUploadImageData(t *testing.T) {
 	t.Parallel()
 
@@ -110,40 +191,12 @@ func TestUploadImageData(t *testing.T) {
 		},
 	}
 
-	tarballReader := func(filename string, content []byte) func(t *testing.T) io.Reader {
-		return func(t *testing.T) io.Reader {
-			t.Helper()
-
-			var buf bytes.Buffer
-
-			gzipWriter := gzip.NewWriter(&buf)
-
-			tarWriter := tar.NewWriter(gzipWriter)
-
-			tarHeader := &tar.Header{
-				Name: filename,
-				Mode: 0600,
-				Size: int64(len(content)),
-			}
-
-			require.NoError(t, tarWriter.WriteHeader(tarHeader))
-
-			_, err := tarWriter.Write(content)
-			require.NoError(t, err)
-
-			require.NoError(t, tarWriter.Close())
-			require.NoError(t, gzipWriter.Close())
-
-			return &buf
-		}
-	}
-
 	rawFileContent := make([]byte, 512)
 	rawFileContent[510], rawFileContent[511] = 0x55, 0xAA
-	fakeRawDiskReader := tarballReader("disk.raw", rawFileContent)
+	fakeRawDiskReader := tarballedReader(files{"disk.raw": rawFileContent})
 
 	qcow2FileContent := []byte("QFI\xfb")
-	fakeQcow2Reader := tarballReader("disk.qcow2", qcow2FileContent)
+	fakeQcow2Reader := tarballedReader(files{"disk.qcow2": qcow2FileContent})
 
 	type TestCase struct {
 		Name              string
@@ -157,153 +210,37 @@ func TestUploadImageData(t *testing.T) {
 
 	testCases := []TestCase{
 		{
-			Name: "fails to create gzip reader",
-			ReaderSetupFunc: func(t *testing.T) io.Reader {
-				t.Helper()
-
-				data := []byte("this is not valid gzip data")
-
-				return bytes.NewBuffer(data)
-			},
-			ExpectedError: true,
-			ExpectedImage: nil,
+			Name:            "fails to create gzip reader",
+			ReaderSetupFunc: stringReader("this is not valid gzip data"),
+			ExpectedError:   true,
+			ExpectedImage:   nil,
 		},
 		{
-			Name: "fails to read invalid tar data",
-			ReaderSetupFunc: func(t *testing.T) io.Reader {
-				t.Helper()
-
-				var buf bytes.Buffer
-
-				gzipWriter := gzip.NewWriter(&buf)
-
-				data := []byte("this is not valid tar data")
-				if _, err := gzipWriter.Write(data); err != nil {
-					require.NoError(t, err)
-				}
-
-				if err := gzipWriter.Close(); err != nil {
-					require.NoError(t, err)
-				}
-
-				return &buf
-			},
-			ExpectedError: true,
-			ExpectedImage: nil,
+			Name:            "fails to read invalid tar data",
+			ReaderSetupFunc: gzippedReader(bytes.NewBuffer(([]byte{1, 2, 3, 4, 5, 6, 7, 8, 9}))),
+			ExpectedError:   true,
+			ExpectedImage:   nil,
 		},
 		{
 			Name: "fails to parse tar header",
-			ReaderSetupFunc: func(t *testing.T) io.Reader {
-				t.Helper()
-
-				base64Data := "EwMwMBMDLTgyMTk1MDI5NnQTE4NzfINzEzAwcXfhZrsDMDAwAAAAEDAxMRNz9DEwMTAwdBMTg3N8g3NzADATc3yDc3P0eFMTc/QxMDEwMHQTE4NzfINzc/QwE3N8g3Nz9HFTMNR0MBMwMHEw9DAAAAAQMDGAABAwMTETc/QxMDH0MHQTMDBx1OFmuwMAAAD/gICAADExNTYyMQAgMBP///+AMTAwdHhTMDB0MBMwMHF3MDEwMTAwdBMTg3N8g3Nz9HhTMDB0MBMwMHF34Wa7AzAwMAAAABAwMTETc/QxMDEwMHQTE4NzfINzc/QwE3N8g3Nz9HhTE3P0MTAxMDB0ExODc3yDc3MwMBNzfINzczB4UzAwdDATMDBxMDAAgAAAEDAxc/QxMDEwMHQTAAAAIOFmuwMwNAAAABAwMTET////gDEwMHR4UzAwdDATMDBxd+FmuwMwMDAAAAAQMDExE3ODc3P0eFMTc/QxMDEwMHQTE4NzfINzc/QzMTEwMzM2MjQ4NDYxMjgzODBzfINzc/R4UzAwdDATMDBxMDAwAAAAEDAxAAAQMDExE3P0MTAxMDB0EzAwcdThZrsDMDQAAAAQg3N8g3Nz9DATc3yDc3P0eFMwMHQwEzAwcTAwMAAAABAwMQAAEDAxMRNz9DEwMTAwdBMwMHgw4Wa7AwAAEDA="
-
-				bs, err := base64.StdEncoding.DecodeString(base64Data)
-				if err != nil {
-					require.NoError(t, err)
-				}
-
-				var buf bytes.Buffer
-
-				gzipWriter := gzip.NewWriter(&buf)
-
-				if _, err := gzipWriter.Write(bs); err != nil {
-					require.NoError(t, err)
-				}
-
-				if err := gzipWriter.Close(); err != nil {
-					require.NoError(t, err)
-				}
-
-				return &buf
-			},
+			ReaderSetupFunc: gzippedReader(
+				base64Reader(t, "EwMwMBMDLTgyMTk1MDI5NnQTE4NzfINzEzAwcXfhZrsDMDAwAAAAEDAxMRNz9DEwMTAwdBMTg3N8g3NzADATc3yDc3P0eFMTc/QxMDEwMHQTE4NzfINzc/QwE3N8g3Nz9HFTMNR0MBMwMHEw9DAAAAAQMDGAABAwMTETc/QxMDH0MHQTMDBx1OFmuwMAAAD/gICAADExNTYyMQAgMBP///+AMTAwdHhTMDB0MBMwMHF3MDEwMTAwdBMTg3N8g3Nz9HhTMDB0MBMwMHF34Wa7AzAwMAAAABAwMTETc/QxMDEwMHQTE4NzfINzc/QwE3N8g3Nz9HhTE3P0MTAxMDB0ExODc3yDc3MwMBNzfINzczB4UzAwdDATMDBxMDAAgAAAEDAxc/QxMDEwMHQTAAAAIOFmuwMwNAAAABAwMTET////gDEwMHR4UzAwdDATMDBxd+FmuwMwMDAAAAAQMDExE3ODc3P0eFMTc/QxMDEwMHQTE4NzfINzc/QzMTEwMzM2MjQ4NDYxMjgzODBzfINzc/R4UzAwdDATMDBxMDAwAAAAEDAxAAAQMDExE3P0MTAxMDB0EzAwcdThZrsDMDQAAAAQg3N8g3Nz9DATc3yDc3P0eFMwMHQwEzAwcTAwMAAAABAwMQAAEDAxMRNz9DEwMTAwdBMwMHgw4Wa7AwAAEDA=")),
 			ExpectedError: true,
 			ExpectedImage: nil,
 		},
 		{
 			Name: "fails to parse oversized tar header",
-			ReaderSetupFunc: func(t *testing.T) io.Reader {
-				t.Helper()
-
-				base64Data := "QlpoOTFBWSZTWcRfsUcAAFDfkOiQQAH/xgBASEBmQF5AAEAICCAAhBtRU9NNIPUaNPUeppo0PUCqVD1AaADEZNHiiS+Wdi697kCxx9qIT3GF8mQnSKvg2sFNkHGCdCG6MqN4zcKTytwhtItXKSj3rOWtbdRUbOOwCADGRCIAjQnVWdKV05Sa6fk0smrPvHAIAfxdyRThQkMRfsUc"
-
-				bs, err := base64.StdEncoding.DecodeString(base64Data)
-				if err != nil {
-					require.NoError(t, err)
-				}
-
-				data := bzip2.NewReader(bytes.NewReader(bs))
-
-				var buf bytes.Buffer
-
-				gzipWriter := gzip.NewWriter(&buf)
-
-				//nolint:gosec
-				if _, err := io.Copy(gzipWriter, data); err != nil {
-					require.NoError(t, err)
-				}
-
-				if err := gzipWriter.Close(); err != nil {
-					require.NoError(t, err)
-				}
-
-				return &buf
-			},
+			ReaderSetupFunc: gzippedReader(
+				bzip2.NewReader(base64Reader(t, "QlpoOTFBWSZTWcRfsUcAAFDfkOiQQAH/xgBASEBmQF5AAEAICCAAhBtRU9NNIPUaNPUeppo0PUCqVD1AaADEZNHiiS+Wdi697kCxx9qIT3GF8mQnSKvg2sFNkHGCdCG6MqN4zcKTytwhtItXKSj3rOWtbdRUbOOwCADGRCIAjQnVWdKV05Sa6fk0smrPvHAIAfxdyRThQkMRfsUc"))),
 			ExpectedError: true,
 			ExpectedImage: nil,
 		},
 		{
 			Name: "fails to process multiple disk files",
-			ReaderSetupFunc: func(t *testing.T) io.Reader {
-				t.Helper()
-
-				var buf bytes.Buffer
-
-				gzipWriter := gzip.NewWriter(&buf)
-
-				tarWriter := tar.NewWriter(gzipWriter)
-
-				tarHeader := &tar.Header{
-					Typeflag: tar.TypeDir,
-					Name:     "empty-dir/",
-					Mode:     0755,
-				}
-
-				if err := tarWriter.WriteHeader(tarHeader); err != nil {
-					require.NoError(t, err)
-				}
-
-				files := map[string][]byte{
-					"disk.raw":   rawFileContent,
-					"disk.qcow2": qcow2FileContent,
-				}
-
-				for name, content := range files {
-					tarHeader = &tar.Header{
-						Name: name,
-						Mode: 0600,
-						Size: int64(len(content)),
-					}
-
-					if err := tarWriter.WriteHeader(tarHeader); err != nil {
-						require.NoError(t, err)
-					}
-
-					if _, err := tarWriter.Write(content); err != nil {
-						require.NoError(t, err)
-					}
-				}
-
-				if err := tarWriter.Close(); err != nil {
-					require.NoError(t, err)
-				}
-
-				if err := gzipWriter.Close(); err != nil {
-					require.NoError(t, err)
-				}
-
-				return &buf
-			},
+			ReaderSetupFunc: tarballedReader(files{
+				"disk.raw":   rawFileContent,
+				"disk.qcow2": qcow2FileContent,
+			}),
 			ExpectedError: true,
 			ExpectedImage: nil,
 		},
@@ -315,13 +252,13 @@ func TestUploadImageData(t *testing.T) {
 
 				return ctx
 			},
-			ReaderSetupFunc: tarballReader("disk.raw", []byte("disk contents")),
+			ReaderSetupFunc: tarballedReader(files{"disk.raw": rawFileContent}),
 			ExpectedError:   true,
 			ExpectedImage:   nil,
 		},
 		{
 			Name:            "fails to find disk file in tar archive",
-			ReaderSetupFunc: tarballReader("randomfile.txt", []byte("file contents")),
+			ReaderSetupFunc: tarballedReader(files{"randomfile.txt": []byte("file contents")}),
 			ExpectedError:   true,
 			ExpectedImage:   nil,
 		},
@@ -329,17 +266,8 @@ func TestUploadImageData(t *testing.T) {
 			Name:            "fails to upload image due to upload conflict",
 			ReaderSetupFunc: fakeRawDiskReader,
 			ProviderSetupFunc: func(provider *mock.MockProvider) {
-				expectedReader := gomock.Cond[io.Reader](func(reader io.Reader) bool {
-					actual, err := io.ReadAll(reader)
-					if err != nil {
-						return false
-					}
-
-					return bytes.Equal(actual, rawFileContent)
-				})
-
 				provider.EXPECT().
-					UploadImage(gomock.Any(), imageID, expectedReader).
+					UploadImage(gomock.Any(), imageID, expectedReaderBytes(rawFileContent)).
 					Return(gophercloud.ErrUnexpectedResponseCode{Actual: http.StatusConflict})
 			},
 			ExpectedError: true,
@@ -349,17 +277,8 @@ func TestUploadImageData(t *testing.T) {
 			Name:            "fails to upload image due to unexpected error",
 			ReaderSetupFunc: fakeRawDiskReader,
 			ProviderSetupFunc: func(provider *mock.MockProvider) {
-				expectedReader := gomock.Cond[io.Reader](func(reader io.Reader) bool {
-					actual, err := io.ReadAll(reader)
-					if err != nil {
-						return false
-					}
-
-					return bytes.Equal(actual, rawFileContent)
-				})
-
 				provider.EXPECT().
-					UploadImage(gomock.Any(), imageID, expectedReader).
+					UploadImage(gomock.Any(), imageID, expectedReaderBytes(rawFileContent)).
 					Return(gophercloud.ErrUnexpectedResponseCode{Actual: http.StatusInternalServerError})
 			},
 			ExpectedError: true,
@@ -369,17 +288,8 @@ func TestUploadImageData(t *testing.T) {
 			Name:            "fails to finalize image",
 			ReaderSetupFunc: fakeRawDiskReader,
 			ProviderSetupFunc: func(provider *mock.MockProvider) {
-				expectedReader := gomock.Cond[io.Reader](func(reader io.Reader) bool {
-					actual, err := io.ReadAll(reader)
-					if err != nil {
-						return false
-					}
-
-					return bytes.Equal(actual, rawFileContent)
-				})
-
 				provider.EXPECT().
-					UploadImage(gomock.Any(), imageID, expectedReader).
+					UploadImage(gomock.Any(), imageID, expectedReaderBytes(rawFileContent)).
 					Return(nil)
 
 				provider.EXPECT().
@@ -393,17 +303,8 @@ func TestUploadImageData(t *testing.T) {
 			Name:            "succeeds in uploading raw image data",
 			ReaderSetupFunc: fakeRawDiskReader,
 			ProviderSetupFunc: func(provider *mock.MockProvider) {
-				expectedReader := gomock.Cond(func(reader io.Reader) bool {
-					actual, err := io.ReadAll(reader)
-					if err != nil {
-						return false
-					}
-
-					return bytes.Equal(actual, rawFileContent)
-				})
-
 				provider.EXPECT().
-					UploadImage(gomock.Any(), imageID, expectedReader).
+					UploadImage(gomock.Any(), imageID, expectedReaderBytes(rawFileContent)).
 					Return(nil)
 
 				provider.EXPECT().
@@ -417,17 +318,8 @@ func TestUploadImageData(t *testing.T) {
 			Name:            "succeeds in uploading qcow2 image data",
 			ReaderSetupFunc: fakeQcow2Reader,
 			ProviderSetupFunc: func(provider *mock.MockProvider) {
-				expectedReader := gomock.Cond(func(reader io.Reader) bool {
-					actual, err := io.ReadAll(reader)
-					if err != nil {
-						return false
-					}
-
-					return bytes.Equal(actual, qcow2FileContent)
-				})
-
 				provider.EXPECT().
-					UploadImage(gomock.Any(), imageID, expectedReader).
+					UploadImage(gomock.Any(), imageID, expectedReaderBytes(qcow2FileContent)).
 					Return(nil)
 
 				provider.EXPECT().
