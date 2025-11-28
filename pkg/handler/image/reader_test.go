bd920ebc9d63a2d8dd0401ccd301cadf72f048c8
Michael Bridgen <michael.bridgen@nscale.com>
2025-11-28 13:50:03 +0000

    Implement Image client used by handler
    The client used by the HTTP handler adapts between the requests and
    the provider; the interfaces are fairly similar, but there's a bit of
    validation and dispatch to do.
    
    This commit fills in the client methods. The create image handler uses
    the saga pattern. There's really only one retractable operation
    (creating an image record in the provider) for now, but since
    
     1. it can occur in the middle of the whole thing; and,
     2. at some point there will be allocations to allocate,
    
    it's useful to be able to retract things.
    
    This commit also adds tests, which use a mock to stand in for the
    provider.
    
    I've left room in `upload.go` for handling different forms the
    uploaded file might come in. The initial form supported is a gzipped
    tarball; we can extend this by swapping out the extraction step for
    something else, e.g., just decompressing the input stream; and, we can
    make decompression contingent on Content-Encoding, or the file name,
    and MIME type (though more of those might need to be passed through
    from the handler method).
    
    On reader_test.go:
    
    We use Peek(), but I don't want to allocate the exact number needed
    for that because it's likely to be too slow. I'd rather let the buffer
    be the default size; but it needs to be bigger than we need, so we
    don't just get an error. This test checks that assumption.
    
    Co-authored-by: alansy-nscale <alan.sy@nscale.com>

---
+++ pkg/handler/image/reader_test.go
@@ -0,0 +1,49 @@
+/*
+Copyright 2025 the Unikorn Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package image_test
+
+import (
+	"bufio"
+	"bytes"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// This checks that it's OK for us to `Peek(...)“ as much as we need to. The docs for `Peek()` say
+//
+// > If Peek returns fewer than n bytes, it also returns an error explaining why the read is short.
+// > The error is ErrBufferFull if n is larger than b's buffer size.
+//
+//	https://pkg.go.dev/bufio#Reader.Peek
+//
+// We create the buffer using bufio.NewReader, which will use the default size (if we used exactly
+// what we needed, reads could be much less inefficient). The default size is in
+//
+//	https://cs.opensource.google/go/go/+/refs/tags/go1.25.5:src/bufio/bufio.go;l=19
+//
+// but not exported so we can't check it directly. It's currently big enough for our purposes;
+// this is an early warning of that changing.
+func TestBufioDefaultSize(t *testing.T) {
+	t.Parallel()
+
+	underlying := bytes.NewBuffer(make([]byte, 10000))
+	buffered := bufio.NewReader(underlying)
+
+	_, err := buffered.Peek(512) // the most we peek, from the MBR validation.
+	require.NoError(t, err)
+}
---
+++ pkg/handler/image/upload.go
@@ -0,0 +1,149 @@
+/*
+Copyright 2025 the Unikorn Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package image
+
+import (
+	"archive/tar"
+	"compress/gzip"
+	"context"
+	goerrors "errors"
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/unikorn-cloud/core/pkg/server/errors"
+	"github.com/unikorn-cloud/region/pkg/providers/types"
+
+	"sigs.k8s.io/controller-runtime/pkg/log"
+)
+
+//nolint:cyclop // I consider this easy enough to grok.
+func extractFileFromTarball(ctx context.Context, data io.Reader, format types.ImageDiskFormat) (*os.File, error) {
+	var (
+		expectedFilename     string
+		validatingReaderFunc func(io.Reader) (io.Reader, error)
+	)
+
+	switch format {
+	case types.ImageDiskFormatRaw:
+		expectedFilename = "disk.raw"
+		validatingReaderFunc = NewMasterBootRecordReader
+	case types.ImageDiskFormatQCOW2:
+		expectedFilename = "disk.qcow2"
+		validatingReaderFunc = NewQCOW2Reader
+	default:
+		return nil, errors.OAuth2InvalidRequest("unhandled disk format").WithValues("disk_format", format)
+	}
+
+	var (
+		tarReader = tar.NewReader(data)
+	)
+
+	var stagedFile *os.File
+
+	for {
+		// This gives the context a chance to be cancelled; potentially we're reading a lot of bytes here.
+		select {
+		case <-ctx.Done():
+			return nil, ctx.Err()
+		default:
+		}
+
+		header, err := tarReader.Next()
+
+		if goerrors.Is(err, io.EOF) {
+			break
+		}
+
+		if err != nil {
+			return nil, err
+		}
+
+		if header.Typeflag != tar.TypeReg {
+			continue
+		}
+
+		if header.Name == expectedFilename {
+			validatingReader, err := validatingReaderFunc(tarReader)
+			if err != nil {
+				return nil, err
+			}
+
+			tempFile, err := os.CreateTemp(os.TempDir(), "disk_")
+			if err != nil {
+				return nil, err
+			}
+
+			if _, err = io.Copy(tempFile, validatingReader); err != nil {
+				return nil, err
+			}
+
+			// We've written to the end of the file; now reset to the beginning to read it.
+			if _, err = tempFile.Seek(0, io.SeekStart); err != nil {
+				return nil, err
+			}
+
+			stagedFile = tempFile
+			// There should be only one entry in the tar file with the expected name. It's _possible_ for
+			// tar files to have repeated names (see https://en.wikipedia.org/wiki/Tar_(computing)). For
+			// simplicity, here I'm assuming that's not a legitimate use.
+			break
+		}
+	}
+
+	if stagedFile == nil {
+		return nil, errors.OAuth2InvalidRequest("The provided file does not contain a disk image of the expected format").WithValues("format", format)
+	}
+
+	return stagedFile, nil
+}
+
+func uploadImageData(ctx context.Context, imageID string, diskFormat types.ImageDiskFormat, sourceReader io.Reader, provider provider) error {
+	gzipReader, err := gzip.NewReader(sourceReader)
+	if err != nil {
+		return errors.OAuth2ServerError("The server encountered an unexpected error while receiving the image data").WithError(err)
+	}
+
+	sourceReader = gzipReader
+	defer gzipReader.Close()
+
+	// Stage the image upload into a temp file, so that we can close the request without
+	// relying on our own upstream call completing.
+	staged, err := extractFileFromTarball(ctx, sourceReader, diskFormat)
+	if err != nil {
+		return errors.OAuth2InvalidRequest("extracting file from tarball").WithError(err)
+	}
+
+	defer func() {
+		staged.Close()
+
+		if err := os.Remove(staged.Name()); err != nil {
+			log.FromContext(ctx).Error(err, "failed to remove temporary disk image file", "file", staged.Name())
+		}
+	}()
+
+	if err = provider.UploadImageData(ctx, imageID, staged); err != nil {
+		if goerrors.Is(err, types.ErrImageNotReadyForUpload) {
+			err = fmt.Errorf("%w: image data has already been uploaded", ErrProviderResource)
+			return errors.HTTPConflict().WithError(err)
+		}
+
+		return errors.OAuth2ServerError("The server encountered an unexpected error while uploading the image data").WithError(err)
+	}
+
+	return nil
+}
