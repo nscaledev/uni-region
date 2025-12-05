08f0bcf2afa15f97e9025fe14f91449505faf3f3
schristoff <28318173+schristoff@users.noreply.github.com>
2025-12-05 11:39:25 +0100

    remove action items
    Signed-off-by: schristoff <28318173+schristoff@users.noreply.github.com>

--- pkg/handler/storage/client.go
+++ pkg/handler/storage/client.go
@@ -86,7 +86,6 @@ func (c *Client) ListV2(ctx context.Context, params openapi.GetApiV2FilestorageP
 	selector, err = rbac.AddOrganizationAndProjectIDQuery(ctx, selector, util.OrganizationIDQuery(params.OrganizationID), util.ProjectIDQuery(params.ProjectID))
 	if err != nil {
 		if rbac.HasNoMatches(err) {
-			//todo(schristoff): discuss
 			return nil, nil
 		}
 
