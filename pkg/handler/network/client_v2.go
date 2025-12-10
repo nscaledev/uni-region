89b6f5b652754f07f50369d959dacfbdbca6ad79
Simon Murray <spjmurray@yahoo.co.uk>
2025-12-10 12:52:03 +0000

    Quick Fix to Network Allocations
    Modifying a network does not preserve the allocation ID.  This is a
    temporary fix in lieu of a better solution.

--- pkg/handler/network/client_v2.go
+++ pkg/handler/network/client_v2.go
@@ -424,6 +424,12 @@ func (c *Client) Update(ctx context.Context, networkID string, request *openapi.
 		return nil, err
 	}
 
+	if required.Annotations == nil {
+		required.Annotations = map[string]string{}
+	}
+
+	required.Annotations[coreconstants.AllocationAnnotation] = current.Annotations[coreconstants.AllocationAnnotation]
+
 	updated := current.DeepCopy()
 	updated.Labels = required.Labels
 	updated.Annotations = required.Annotations
