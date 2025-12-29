74e03211bbe0249d87c81a8b7b3dd9fcecfa844e
Ricardo R <ricardo.rauber@nscale.com>
2025-12-30 10:50:31 +1300

    chore: add attachment status tracking to network detachment operations and move helper to dedicated status.go file

--- pkg/provisioners/managers/file-storage/provision.go
+++ pkg/provisioners/managers/file-storage/provision.go
@@ -178,7 +178,7 @@ func (p *Provisioner) detachStaleNetworks(ctx context.Context, cli client.Client
 		log.V(1).Info("detaching network", "vlan", vlan)
 
 		if err := driver.DetachNetwork(ctx, p.fileStorage.Labels[coreconstants.ProjectLabel], p.fileStorage.Name, vlan); err != nil {
-			setVLanAttachmentStatus(p.fileStorage, vlan, unikornv1.AttachmentProvisioned, err.Error())
+			setVLanAttachmentStatus(p.fileStorage, vlan, unikornv1.AttachmentErrored, err.Error())
 
 			return err
 		}
@@ -228,69 +228,4 @@ func (p *Provisioner) isNetworkStillAttached(networkName string) bool {
 	}
 
 	return false
-}
-
-// setNetworkAttachmentStatus sets the corresponding attachment status returns true if the status are changed by this call.
-func setNetworkAttachmentStatus(fileStorage *unikornv1.FileStorage, networkID string, segmentationID *int, status unikornv1.AttachmentProvisioningStatus, message string) {
-	if fileStorage == nil {
-		return
-	}
-
-	matchNetworkID := func(as unikornv1.FileStorageAttachmentStatus) bool {
-		return as.NetworkID == networkID
-	}
-
-	i := slices.IndexFunc(fileStorage.Status.Attachments, matchNetworkID)
-	if i < 0 {
-		fileStorage.Status.Attachments = append(fileStorage.Status.Attachments, unikornv1.FileStorageAttachmentStatus{
-			NetworkID:          networkID,
-			SegmentationID:     segmentationID,
-			ProvisioningStatus: status,
-			Message:            message,
-		})
-
-		return
-	}
-
-	fileStorage.Status.Attachments[i].ProvisioningStatus = status
-	fileStorage.Status.Attachments[i].SegmentationID = segmentationID
-	fileStorage.Status.Attachments[i].Message = message
-}
-
-// setVLanAttachmentStatus sets the corresponding attachment status returns true if the status are changed by this call.
-func setVLanAttachmentStatus(fileStorage *unikornv1.FileStorage, segmentationID int, status unikornv1.AttachmentProvisioningStatus, message string) {
-	if fileStorage == nil {
-		return
-	}
-
-	matchSegmentationID := func(as unikornv1.FileStorageAttachmentStatus) bool {
-		return *as.SegmentationID == segmentationID
-	}
-
-	i := slices.IndexFunc(fileStorage.Status.Attachments, matchSegmentationID)
-	if i < 0 {
-		return
-	}
-
-	fileStorage.Status.Attachments[i].ProvisioningStatus = status
-	fileStorage.Status.Attachments[i].Message = message
-}
-
-// removeAttachmentStatus removes the attachment status for the given network ID.
-func removeAttachmentStatus(fileStorage *unikornv1.FileStorage, segmentationID int) {
-	if fileStorage == nil || len(fileStorage.Status.Attachments) == 0 {
-		return
-	}
-
-	// If there no attachment for the given network ID, return false.
-	matchSegmentationID := func(as unikornv1.FileStorageAttachmentStatus) bool {
-		return *as.SegmentationID == segmentationID
-	}
-
-	i := slices.IndexFunc(fileStorage.Status.Attachments, matchSegmentationID)
-	if i < 0 {
-		return
-	}
-
-	fileStorage.Status.Attachments = slices.Delete(fileStorage.Status.Attachments, i, i+1)
 }
