aacfa9431e066d00b908f149a25f21cc8a34da82
Simon Murray <spjmurray@yahoo.co.uk>
2026-01-05 09:40:18 +0000

    Update License (#298)
    New year, new validity period...

--- pkg/handler/options.go
+++ pkg/handler/options.go
@@ -1,6 +1,7 @@
 /*
 Copyright 2022-2024 EscherCloud.
 Copyright 2024-2025 the Unikorn Authors.
+Copyright 2026 Nscale.
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
