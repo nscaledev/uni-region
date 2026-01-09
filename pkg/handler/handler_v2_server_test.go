7d1df907d6be0aa38f885090848766c788e772d2
Michael Bridgen <michael.bridgen@nscale.com>
2026-01-09 14:06:17 +0000

    Plumb API handler through to client, and test pipes
    The tests check:
    
     - if there's a known good configuration in place, you still have to
       have permissions;
     - if you do have permissions, you'll get the "not implemented"
       response the client currently returns.

--- pkg/handler/handler_v2_server_test.go
+++ pkg/handler/handler_v2_server_test.go
@@ -29,10 +29,12 @@ import (
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 
+	"github.com/unikorn-cloud/core/pkg/constants"
 	identityv1 "github.com/unikorn-cloud/identity/pkg/apis/unikorn/v1alpha1"
 	identityapi "github.com/unikorn-cloud/identity/pkg/openapi"
 	"github.com/unikorn-cloud/identity/pkg/rbac"
 	regionv1 "github.com/unikorn-cloud/region/pkg/apis/unikorn/v1alpha1"
+	regionconstants "github.com/unikorn-cloud/region/pkg/constants"
 	"github.com/unikorn-cloud/region/pkg/openapi"
 
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -57,32 +59,52 @@ func fakeClientWithSchema(t *testing.T, objects ...client.Object) client.Client 
 	return b.Build()
 }
 
-func contextWithOrgPerms(t *testing.T, orgID, endpoint string, ops []identityapi.AclOperation) context.Context {
-	t.Helper()
-
-	acl := &identityapi.Acl{
-		Organizations: &identityapi.AclOrganizationList{
-			identityapi.AclOrganization{
-				Id: orgID,
-				Endpoints: &identityapi.AclEndpoints{
-					identityapi.AclEndpoint{
-						Name:       endpoint,
-						Operations: ops,
-					},
-				},
-			},
-		},
-	}
-
-	return rbac.NewContext(t.Context(), acl)
+type aclBuilder struct {
+	org identityapi.AclOrganization
 }
 
-func newServer(t *testing.T, name string) *regionv1.Server {
+func newOrganisationACLBuilder(orgID string) *aclBuilder {
+	return &aclBuilder{
+		org: identityapi.AclOrganization{
+			Id: orgID,
+		},
+	}
+}
+
+func (b *aclBuilder) addEndpoint(endpoint string, perms ...identityapi.AclOperation) *aclBuilder {
+	var endpoints []identityapi.AclEndpoint
+	if ep := b.org.Endpoints; ep != nil {
+		endpoints = *ep
+	}
+
+	endpoints = append(endpoints, identityapi.AclEndpoint{
+		Name:       endpoint,
+		Operations: perms,
+	})
+
+	b.org.Endpoints = &endpoints
+
+	return b
+}
+
+func (b *aclBuilder) buildContext(ctx context.Context) context.Context {
+	return rbac.NewContext(ctx, &identityapi.Acl{
+		Organizations: &identityapi.AclOrganizationList{b.org},
+	})
+}
+
+func newServer(t *testing.T, name, namespace, orgID, projectID string) *regionv1.Server {
 	t.Helper()
 
 	return &regionv1.Server{
 		ObjectMeta: metav1.ObjectMeta{
-			Name: name,
+			Name:      name,
+			Namespace: namespace,
+			Labels: map[string]string{
+				constants.OrganizationLabel:             orgID,
+				constants.ProjectLabel:                  projectID,
+				regionconstants.ResourceAPIVersionLabel: "2", // necessary for GetRawV2 to "find" this object
+			},
 		},
 	}
 }
@@ -114,7 +136,9 @@ func TestServerV2_EmptyList(t *testing.T) {
 	c := fakeClientWithSchema(t) // NB no objects
 	handler := NewServerV2Handler(c, namespace)
 
-	ctx := contextWithOrgPerms(t, "org1", "region:servers:v2", nil)
+	ctx := newOrganisationACLBuilder("org-empty-list").
+		addEndpoint("region:servers").
+		buildContext(t.Context())
 	response := httptest.NewRecorder()
 	request := httptest.NewRequestWithContext(ctx, http.MethodGet, "/api/v2/servers", nil)
 
@@ -129,10 +153,13 @@ func TestServerV2_NotAllowedList(t *testing.T) {
 
 	namespace := "region-test-home"
 
-	c := fakeClientWithSchema(t, newServer(t, "server1"))
+	c := fakeClientWithSchema(t, newServer(t, "server1", namespace, "org-not-allowed-test", ""))
 	handler := NewServerV2Handler(c, namespace)
 
-	ctx := contextWithOrgPerms(t, "org1", "region:servers:v2", nil)
+	ctx := newOrganisationACLBuilder("org1").
+		addEndpoint("region:servers").
+		buildContext(t.Context())
+
 	response := httptest.NewRecorder()
 	request := httptest.NewRequestWithContext(ctx, http.MethodGet, "/api/v2/servers", nil)
 
@@ -140,4 +167,75 @@ func TestServerV2_NotAllowedList(t *testing.T) {
 
 	require.Equal(t, http.StatusOK, response.Result().StatusCode)
 	assertBodyIsEmptyServerList(t, response.Result().Body)
+}
+
+func TestServerV2_Snapshot_NotAllowedWithoutPermissions(t *testing.T) {
+	t.Parallel()
+
+	namespace := "region-test-home"
+	orgID := "org1"
+
+	c := fakeClientWithSchema(t, newServer(t, "server1", namespace, "org1", ""))
+	handler := NewServerV2Handler(c, namespace)
+
+	testcases := []struct {
+		name    string
+		makeCtx func(context.Context) context.Context
+	}{
+		{
+			name:    "no_perms",
+			makeCtx: newOrganisationACLBuilder(orgID).buildContext,
+		},
+		{
+			name: "only_region_server_read",
+			makeCtx: newOrganisationACLBuilder(orgID).
+				addEndpoint("region:servers", identityapi.Read).
+				buildContext,
+		},
+		{
+			name: "only_image_create",
+			makeCtx: newOrganisationACLBuilder(orgID).
+				addEndpoint("region:images", identityapi.Create).
+				buildContext,
+		},
+	}
+
+	for _, tc := range testcases {
+		t.Run(tc.name, func(t *testing.T) {
+			t.Parallel()
+
+			ctx := tc.makeCtx(t.Context())
+
+			response := httptest.NewRecorder()
+			request := httptest.NewRequestWithContext(ctx, http.MethodGet, "/api/v2/servers", nil)
+
+			handler.PostApiV2ServersServerIDSnapshot(response, request, "server1")
+
+			require.Equal(t, http.StatusForbidden, response.Result().StatusCode)
+		})
+	}
+}
+
+func TestServerV2_Snapshot_HappyPath(t *testing.T) {
+	t.Parallel()
+
+	namespace := "region-test-home"
+	orgID := "org1"
+	projectID := "project1"
+
+	c := fakeClientWithSchema(t, newServer(t, "server1", namespace, orgID, projectID))
+	handler := NewServerV2Handler(c, namespace)
+
+	ctx := newOrganisationACLBuilder(orgID).
+		addEndpoint("region:images", identityapi.Create).
+		addEndpoint("region:servers", identityapi.Read).
+		buildContext(t.Context())
+
+	response := httptest.NewRecorder()
+	request := httptest.NewRequestWithContext(ctx, http.MethodGet, "/api/v2/servers", nil)
+
+	handler.PostApiV2ServersServerIDSnapshot(response, request, "server1")
+
+	// fixme: this is just testing that it hits the stub, for the minute
+	require.Equal(t, http.StatusUnprocessableEntity, response.Result().StatusCode)
 }
