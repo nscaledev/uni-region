b392879d4816faab6b576137ff97cb563b5a3c82
Michael Bridgen <michael.bridgen@nscale.com>
2026-01-28 15:06:29 +0000

    Factor out network handler
    Similar to the previous commit factoring out a handler for the
    identity API endpoint methods, this factors out the network API
    endpoint handlers.
    
    One difference is that there's v1 and v2 endpoints; since these use
    the same client, they can all go on the same struct (though I've put
    network v1 and v2 methods in their own files, since I started doing
    that with the server API).

--- pkg/handler/handler_v2.go
+++ pkg/handler/handler_v2.go
@@ -27,69 +27,6 @@ import (
 	"github.com/unikorn-cloud/region/pkg/openapi"
 )
 
-func (h *Handler) GetApiV2Networks(w http.ResponseWriter, r *http.Request, params openapi.GetApiV2NetworksParams) {
-	result, err := h.networkClient().ListV2(r.Context(), params)
-	if err != nil {
-		errors.HandleError(w, r, err)
-		return
-	}
-
-	util.WriteJSONResponse(w, r, http.StatusOK, result)
-}
-
-func (h *Handler) PostApiV2Networks(w http.ResponseWriter, r *http.Request) {
-	request := &openapi.NetworkV2Create{}
-
-	if err := util.ReadJSONBody(r, request); err != nil {
-		errors.HandleError(w, r, err)
-		return
-	}
-
-	result, err := h.networkClient().CreateV2(r.Context(), request)
-	if err != nil {
-		errors.HandleError(w, r, err)
-		return
-	}
-
-	util.WriteJSONResponse(w, r, http.StatusCreated, result)
-}
-
-func (h *Handler) GetApiV2NetworksNetworkID(w http.ResponseWriter, r *http.Request, networkID openapi.NetworkIDParameter) {
-	result, err := h.networkClient().GetV2(r.Context(), networkID)
-	if err != nil {
-		errors.HandleError(w, r, err)
-		return
-	}
-
-	util.WriteJSONResponse(w, r, http.StatusOK, result)
-}
-
-func (h *Handler) PutApiV2NetworksNetworkID(w http.ResponseWriter, r *http.Request, networkID openapi.NetworkIDParameter) {
-	request := &openapi.NetworkV2Update{}
-
-	if err := util.ReadJSONBody(r, request); err != nil {
-		errors.HandleError(w, r, err)
-		return
-	}
-
-	result, err := h.networkClient().Update(r.Context(), networkID, request)
-	if err != nil {
-		errors.HandleError(w, r, err)
-		return
-	}
-
-	util.WriteJSONResponse(w, r, http.StatusAccepted, result)
-}
-
-func (h *Handler) DeleteApiV2NetworksNetworkID(w http.ResponseWriter, r *http.Request, networkID openapi.NetworkIDParameter) {
-	if err := h.networkClient().DeleteV2(r.Context(), networkID); err != nil {
-		errors.HandleError(w, r, err)
-		return
-	}
-
-	w.WriteHeader(http.StatusAccepted)
-}
-
 func (h *Handler) GetApiV2Securitygroups(w http.ResponseWriter, r *http.Request, params openapi.GetApiV2SecuritygroupsParams) {
 	result, err := h.securityGroupClient().ListV2(r.Context(), params)
 	if err != nil {
