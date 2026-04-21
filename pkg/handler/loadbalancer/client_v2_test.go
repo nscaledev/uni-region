3b578acbe6515f434c9ee133bad8436db09845ee
Matt Pryor <matt.pryor@nscale.com>
2026-04-21 17:04:24 +0100

    Narrow load balancer test changes

--- pkg/handler/loadbalancer/client_v2_test.go
+++ pkg/handler/loadbalancer/client_v2_test.go
@@ -71,6 +71,9 @@ func newLBFakeClientBuilder(t *testing.T, objects ...runtime.Object) *fake.Clien
 	scheme := runtime.NewScheme()
 	require.NoError(t, regionv1.AddToScheme(scheme))
 
+	// These tests exercise identity allocation creation/update, which calls
+	// manager.GenerateResourceReference and needs RESTMapping for custom
+	// resources. The fake client defaults to an empty REST mapper.
 	restMapper := apimeta.NewDefaultRESTMapper([]schema.GroupVersion{regionv1.SchemeGroupVersion})
 	restMapper.Add(regionv1.SchemeGroupVersion.WithKind("Network"), apimeta.RESTScopeNamespace)
 	restMapper.Add(regionv1.SchemeGroupVersion.WithKind("LoadBalancer"), apimeta.RESTScopeNamespace)
