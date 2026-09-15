21af3c89136f00407db188b6f95335801276dca7
Sravan Medarapu <sravan.medarapu@nscale.com>
2026-09-15 21:21:00 +0530

    Restore Fake DC network setting

--- test/api/config.go
+++ test/api/config.go
@@ -32,6 +32,7 @@ type TestConfig struct {
 	RegionCACertPath        string
 	RegionID                string
 	FakeRegionID            string
+	FakeNetworkID           string
 	PrivateRegionID         string
 	SecondaryOrgID          string
 	SecondaryProjectID      string
@@ -97,6 +98,7 @@ func LoadTestConfig() (*TestConfig, error) {
 		ProjectID:               v.GetString("TEST_PROJECT_ID"),
 		RegionID:                v.GetString("TEST_REGION_ID"),
 		FakeRegionID:            v.GetString("FAKE_TEST_REGION_ID"),
+		FakeNetworkID:           v.GetString("FAKE_TEST_NETWORK_ID"),
 		PrivateRegionID:         v.GetString("TEST_PRIVATE_REGION_ID"),
 		SecondaryOrgID:          v.GetString("TEST_SECONDARY_ORG_ID"),
 		SecondaryProjectID:      v.GetString("TEST_SECONDARY_PROJECT_ID"),
